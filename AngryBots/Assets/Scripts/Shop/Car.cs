267739f582b64ef7179f68e16f2fccb9118a7bd5
Alexey Karazhey <grimreio@gmail.com>
2013-09-09 14:39:31 +0300

    imported OpenIAB lib, created basic UI

---
+++ AngryBots/Assets/Scripts/Shop/Car.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+    [SerializeField]
+    float _rotationSpeed = 100.0f;
+
+    [SerializeField]
+    GameObject _simpleCar = null;
+    [SerializeField]
+    GameObject _premiumCar = null;
+
+    void Awake() {
+        _simpleCar.SetActive(false);
+        _premiumCar.SetActive(false);
+    }
+
+    void Update() {
+        transform.Rotate(0, _rotationSpeed * Time.deltaTime, 0);
+    }
+
+    public void SetPremium() {
+        _simpleCar.SetActive(false);
+        _premiumCar.SetActive(true);
+    }
+
+    public void SetRegular() {
+        _simpleCar.SetActive(true);
+        _premiumCar.SetActive(false);
+    }
+}
